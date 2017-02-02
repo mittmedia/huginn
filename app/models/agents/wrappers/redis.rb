0a1cba49b87b69fb4e62d162c5dd1bc0b41009fa
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-02-02 09:03:41 +0100

    Bugfix

--- app/models/agents/wrappers/redis.rb
+++ app/models/agents/wrappers/redis.rb
@@ -1,13 +1,18 @@
-require 'md5'
+require 'digest/md5'
+require 'redis'
 
 module Agents::WRAPPERS::REDIS
-  def redis
-    @redis ||= Redis.connect(url: ENV.fetch('REDIS_URL'))
+
+  def self.redis
+    # @redis ||= Redis.connect(url: ENV.fetch('REDIS_URL'))
+    @redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 15)
   end
 
   def self.digest(key, data)
-    digest = Digest::MD5.hexdigest(data).to_s
-    return false if digest == redis.get(key)
-    redis.set(key, digest)
+    red = redis
+    digest = Digest::MD5.hexdigest(data.to_s).to_s
+    return false if digest == red.get(key)
+    red.set(key, digest)
+    return true
   end
 end
