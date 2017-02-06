99a3c40324eb8d2651b6f0d5d3e77e28d7830b61
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-02-06 11:09:44 +0100

    new module for handling redis

--- app/models/agents/wrappers/redis.rb
+++ app/models/agents/wrappers/redis.rb
@@ -2,16 +2,19 @@ require 'digest/md5'
 require 'redis'
 
 module Agents::WRAPPERS::REDIS
-
   def self.redis
-    # @redis ||= Redis.connect(url: ENV.fetch('REDIS_URL'))
-    @redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 15)
+    @redis ||= Redis.connect(url: ENV.fetch('REDIS_URL'))
+    # @redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 15)
   end
 
   def self.digest(key, data)
     red = redis
+    # red.flushall
     digest = Digest::MD5.hexdigest(data.to_s).to_s
-    return false if digest == red.get(key)
+    # p "=========.#{digest} och #{red.get(key)}"
+    if digest == red.get(key)
+      return false
+    end
     red.set(key, digest)
     return true
   end
