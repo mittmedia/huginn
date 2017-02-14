200afd0f2736197f59edb27f21f08533391e908d
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-02-14 11:06:15 +0100

    New wrapper that uses Google Geocoding API and returns the answer in JSON

---
+++ app/models/agents/wrappers/google_geocoding.rb
@@ -0,0 +1,17 @@
+# In order to build url for Google Geocoding API, you need to provide the following:
+# Format: either "xml" or "json"
+# Address: the search query itself, formatted according to this description: https://developers.google.com/maps/documentation/geocoding/intro#GeocodingRequests
+# API-key from Google's Api Console
+
+module GEOCODE
+  def self.geocode(format, adress, api_key, bounds)
+    url = "https://maps.googleapis.com/maps/api/geocode/#{format}?address=#{adress}&bounds=#{bounds}&key=#{api_key}"
+    # p url
+    uri = URI(URI.escape(url))
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+    data = http.get(uri.request_uri).body
+    JSON.parse(data)
+  end
+end
