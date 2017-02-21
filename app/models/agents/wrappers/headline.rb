4f14ccfd78c7552d50e87c20f8a72aa7db2cc707
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-02-21 15:36:49 +0100

    New module, as helper for vattneninfo_agent.rb

---
+++ app/models/agents/wrappers/headline.rb
@@ -0,0 +1,17 @@
+module Agents::WRAPPERS::Headline
+
+  HEADER = {
+    "Vattenläcka" => "Vattenläcka ",
+    "Ombyggnadsarbete" => "Ombyggnadsarbete  ",
+    "Läcksökning" => "Möjlig vattenläcka ",
+    "Driftstörning" => "Driftstörning "
+  }
+
+  CONTEXT = {
+   "Vattenläcka" => "Läckan gör att påverkade hushåll kan vara utan vatten eller uppleva sämre tryck i kranarna.",
+   "Ombyggnadsarbete" => "",
+   "Läcksökning" => "Berörda hushåll kan uppleva sämre tryck i kranar eller till och med bli utan vatten under en kortare period.",
+   "Driftstörning" => "" 
+  }
+
+end
