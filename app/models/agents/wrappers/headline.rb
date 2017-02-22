9c37f59267ea014b7cb29c1e79eaa1ae7789126b
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-02-22 11:04:00 +0100

    Fixes spelling error

--- app/models/agents/wrappers/headline.rb
+++ app/models/agents/wrappers/headline.rb
@@ -18,7 +18,7 @@ module Agents::WRAPPERS::Headline
     "Vattenläcka" => "MIVA rapporterar att man jobbar med att åtgärda en vättenläcka vid",
     "Ombyggnadsarbete" => "MIVA genomför nu ett planerat ombyggnadsarbete vid",
     "Läcksökning" => "MIVA söker efter en vattenläcka vid",
-    "Driftstörning" => "MIVA raporterar om en driftstörning"
+    "Driftstörning" => "MIVA rapporterar om en driftstörning"
   }
 
 end
