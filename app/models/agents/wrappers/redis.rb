54cfe0cf5e4709a700f009ba5c2c516492acbde1
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-04-07 11:04:30 +0200

    Changes design and adds standalone method for checking for existing keys

--- app/models/agents/wrappers/redis.rb
+++ app/models/agents/wrappers/redis.rb
@@ -3,8 +3,8 @@ require 'redis'
 
 module Agents::WRAPPERS::REDIS
   def self.redis
-    @redis ||= Redis.connect(url: ENV.fetch('REDIS_URL'))
-    # @redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 15)
+    # @redis ||= Redis.connect(url: ENV.fetch('REDIS_URL'))
+    @redis = Redis.new(:host => "127.0.0.1", :port => 5432, :db => 15)
   end
 
   def self.digest(key, data)
@@ -26,6 +26,9 @@ module Agents::WRAPPERS::REDIS
     if !exists?(key)
       digest = Digest::MD5.hexdigest(data.to_s).to_s
       redis.set(key, digest)
+      return true
+    else
+      return false
     end
   end
 
