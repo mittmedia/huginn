7b22850575c3991981a5fa936c3ecb5d0f85117c
Mikael Tjernström <mikael.tjernstrom@mittmedia.se>
2017-04-10 12:44:26 +0200

    Changes from using net/http to httparty

--- app/models/agents/trafikverket/post.rb
+++ app/models/agents/trafikverket/post.rb
@@ -1,11 +1,7 @@
 module Agents::TRAFIKVERKET::POST
 	def self.post_call(url, payload)
 		return if url.empty? || payload.empty?
-    uri = URI.parse url
-    request = Net::HTTP::Post.new uri.path
-    request.body = payload
-    request.content_type = 'text/xml'
-    response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+    response = HTTParty.post url, :body => payload, :headers => {'Content-type' => 'text/xml'}
     JSON.parse(response.body)
 	end
 end
